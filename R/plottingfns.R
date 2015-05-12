119b9976379423e942167344554d03f37eb9b3cf
andrewzm <andrewzm@gmail.com>
2015-05-12 10:18:18 +1000

    Added white borders around LinePlotTheme()

--- R/plottingfns.R
+++ R/plottingfns.R
@@ -287,7 +287,7 @@ LinePlotTheme <- function() {
   g <- ggplot() + theme(panel.background = element_rect(fill='white', colour='black'),text = element_text(size=20),
                         panel.grid.major =  element_line(colour = "light gray", size = 0.05),
                         panel.border  = element_rect(fill=NA, colour='black'),
-                        plot.margin=unit(c(0,0,0,0),"mm")) 
+                        plot.margin=unit(c(5,5,5,0),"mm")) 
   return (g)
 }
 
